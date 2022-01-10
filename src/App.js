0f9b541ce8e57f853b0dd89f323586b0d8b5479e
kavyajayakumar123 <kavyasreekutty57@gmail.com>
2022-01-11 00:14:22 +0530

    backend connected

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import { useState } from "react";
 import "./style.css";
+import axios from "axios";
 
 function App() {
-  const[a,setA]=useState("white");
-  function Dark(){
-    setA("divdark");
+  const [a, setA] = useState("white");
+  function Dark() {
+    axios.get("http://localhost:8080/dark").then((res)=>{
+      setA(res.data);
+    })
 
   }
-  function Dim(){
-    setA("divdim");
+  function Dim() {
+    axios.get("http://localhost:8080/dim").then((res)=>{
+      setA(res.data);
+    })
     
+
   }
-  function Red(){
-    setA("divred");
+  function Red() {
+    axios.get("http://localhost:8080/red").then((res)=>{
+      setA(res.data);
+    })
+
     
+
   }
-  function Light(){
-    setA("divlight");
-    
-  }
-  function Night(){
-    setA("divnight");
-    
+  function Light() {
+    axios.get("http://localhost:8080/light").then((res)=>{
+      setA(res.data);
+    })
+
+
+  } 
+  function Night() {
+    axios.get("http://localhost:8080/night").then((res)=>{
+      setA(res.data);
+    })
+
   }
   return (
     <>
-    <div className={a}>
-      <button onClick={Dark}id="dark">DARK</button>
-      <button onClick={Dim}id="dim">DIM</button>
-      <button onClick={Red}id="red">RED</button>
-      <button onClick={Light}id="light">LIGHT</button>
-      <button onClick={Night}id="night">NIGHT</button>
-      
+      <div className={a}>
+        <button onClick={Dark} id="dark">DARK</button>
+        <button onClick={Dim} id="dim">DIM</button>
+        <button onClick={Red} id="red">RED</button>
+        <button onClick={Light} id="light">LIGHT</button>
+        <button onClick={Night} id="night">NIGHT</button>
 
-    </div>
+
+      </div>
     </>
 
   );
