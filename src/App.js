890b85c7d47c14357c948316d907908cdcb6217d
kavyajayakumar123 <kavyasreekutty57@gmail.com>
2022-01-10 22:16:59 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useState } from "react";
+import "./style.css";
+
 function App() {
+  const[a,setA]=useState("white");
+  function Dark(){
+    setA("divdark");
+
+  }
+  function Dim(){
+    setA("divdim");
+    
+  }
+  function Red(){
+    setA("divred");
+    
+  }
+  function Light(){
+    setA("divlight");
+    
+  }
+  function Night(){
+    setA("divnight");
+    
+  }
   return (
     <>
+    <div className={a}>
+      <button onClick={Dark}id="dark">DARK</button>
+      <button onClick={Dim}id="dim">DIM</button>
+      <button onClick={Red}id="red">RED</button>
+      <button onClick={Light}id="light">LIGHT</button>
+      <button onClick={Night}id="night">NIGHT</button>
+      
 
-
-      <h1>hlooo</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quia mollitia qui, error quaerat ad quisquam?</p>
+    </div>
     </>
 
   );
